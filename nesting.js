aae8ce1eb030741772aa7ca9947067336996263d
Kori Gandolph <korigandolph@Koris-MacBook-Air.local>
2019-12-20 15:43:40 -0700

    first two nesting

--- nesting.js
+++ nesting.js
@@ -51,7 +51,16 @@ var employees = [
 */
 
 //Code Here
-
+function employeeUpdater(){
+  for (let i=0; i<employees.length; i++){
+    if (employees[i].firstName==="Theo"){
+      employees.splice(i,1)
+    }else {if (employees[i].firstName === "Lorie"){
+  employees[i].department = "HR"}
+    }
+  }
+  return employees;
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -68,8 +77,24 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
     3. Return the updated array.
 */
 
-//Code Here
+function removeDuplicates(){
+  var arr = [];
+     var dup = workplaceAccidents;
+  
+     for(let i = 0; i < dup.length; i++){
+       for(let j = dup.length -1; j > i; j--){
+         if(dup[i] === dup[j]){
+           dup.splice(j,1);
+         }
+        
+       }
+     };
+     return dup;
+   }
+  
+   removeDuplicates();
 
+   
 
 
 ////////// PROBLEM 3 //////////
@@ -97,6 +122,7 @@ var cat = {
 */
 
 //Code Here
+
 var grumpyActivity;
 var fluffy2ndFriend;
 
